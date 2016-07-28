800dfa3a1a5541a427f143353942cfa5cfe688dc
gajal <gajal@skillspeed.com>
2016-07-28 06:31:13 -0700

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 	def hello
 	render html: "Hello World"
+	render html: "Hi i am a testing branch"
 	end
 end
