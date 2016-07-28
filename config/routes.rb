4500227499fd3f6f39e391fbdb4236e257e95e7d
gajal <gajal@skillspeed.com>
2016-07-28 08:15:03 -0700

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
 
-	root 'application#hello'
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  resources :microposts
+  resources :users
+	root 'static_pages#home'
 end
